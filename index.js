e96f3ff11e81071b32233826eecd82ab8ef0fe9c
ronniebm <ronnie.coding@gmail.com>
2022-09-17 15:29:33 -0500

    creating 'index.js' file
    * importing express with commonJS syntax.
    * creating an express app instance.
    * setting initial routing '/' to send a string when navigating to URL localhost:3000.
    * running express app with listen.

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+// Creating the express app.
+const app = express()
+
+// Routing
+app.get('/', function(req, res) {
+    res.send('Express Initial config, check URL localhost:3000')
+})
+
+// Defining app port, and starting app.
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`== Server is running on port ${port} ==`)
+})
